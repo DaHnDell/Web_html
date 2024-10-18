1adc31e35c4e85a3671cb8f5b9f331c602dca6d0
DahnDell <khanman1208@gmail.com>
2024-10-18 16:45:00 +0900

    99클래스 수정본 + 배열 수업

--- 03/01/1-2.js
+++ 03/01/1-2.js
@@ -1,16 +1,31 @@
-var name = "윤석렬";
-var age = 60;
-var strings = '대통령' + name + "은 나이가" + age + "쨜~!"
-console.log(strings);
-strings = `
-    대통령 ${name}은 
-    나이가 ${age}살 입니다.
-`
-console.log(strings);
+// var name = "윤석렬";
+// var age = 60;
+// var strings = '대통령' + name + "은 나이가" + age + "쨜~!"
+// console.log(strings);
+// strings = `
+//     대통령 ${name}은 
+//     나이가 ${age}살 입니다.
+// `
+// console.log(strings);
 
-a=10;
-b="10";
+// a=10;
+// b="10";
 
-if(a===b){
-    console.log('haha');
-}
+// if(a===b){
+//     console.log('haha');
+// }
+
+// 배열 
+
+// var arr = []; // 배열 리터럴
+var arr = new Array(10); // 10개의 공간을 가지는 배열
+var arr = new Array(10, 20); // 10과 20을 값으로 가지는 2칸의 배열
+// 자료형이 정해져 있지 않다는 점이 자바와의 가장 큰 차이점이라고 할 수 있음.
+arr[0] = 10;
+arr[1] = 'abcd';
+arr[2] = true;
+arr[3] = undefined;
+arr[4] = [1, 2, 3, 4];
+arr[5] = {x:10, y:20};
+console.log(arr[5].y);
+console.log(arr[4][2]);
